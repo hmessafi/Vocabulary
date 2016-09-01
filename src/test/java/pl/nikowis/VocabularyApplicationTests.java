d7329d2b9bc2e3ea26fd572b653bd53174d6883b
nikowis <nikowis@gmail.com>
2016-09-02 00:24:16 +0200

    Added tests.

--- src/test/java/pl/nikowis/VocabularyApplicationTests.java
+++ src/test/java/pl/nikowis/VocabularyApplicationTests.java
@@ -1,16 +1,65 @@
 package pl.nikowis;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import pl.nikowis.entities.Role;
+import pl.nikowis.entities.User;
+import pl.nikowis.security.UserRoles;
+import pl.nikowis.services.RoleService;
+import pl.nikowis.services.UserService;
+import pl.nikowis.services.WordService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(
+        classes = VocabularyApplication.class
+        , webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 public class VocabularyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private WordService wordService;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void roleServiceTests() {
+        String roleName = "testRole";
+        roleService.save(new Role(roleName));
+        Assert.assertEquals(roleName, roleService.findOneByName(roleName).getName());
+    }
+
+    @Test()
+    public void userServiceTests() {
+        String testUserUsername = "testUser";
+        String testPassword = "1111";
+        String testAdminUsername = "testAdmin";
+        User admin = userService.createNewAdmin(new User(testAdminUsername, testPassword));
+        Assert.assertEquals(testAdminUsername, admin.getUsername());
+        User user = userService.createNewAdmin(new User(testUserUsername, testPassword));
+        Assert.assertEquals(testUserUsername, user.getUsername());
+
+        admin.setPassword(testPassword);
+        User dbAdmin = userService.authenticateUser(admin);
+        Assert.assertEquals(admin.getId(), dbAdmin.getId());
+        Assert.assertEquals(UserRoles.ROLE_ADMIN, dbAdmin.getRole().getName());
+
+        user.setPassword(testPassword);
+        User dbUser = userService.authenticateUser(user);
+        Assert.assertEquals(user.getId(), dbUser.getId());
+        Assert.assertEquals(UserRoles.ROLE_USER, dbUser.getRole().getName());
+    }
+
+    @Test
+    public void wordServiceTests() {
+
+    }
 
 }
